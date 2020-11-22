c251d9ccccf7c1845c2b21e28da40a725a7d7e41
8118557@qq.com <8118557@qq.com>
2020-11-22 15:55:03 +0800

    Initial commit

---
+++ hello.py
@@ -0,0 +1,49 @@
+
+def print_hello():
+    print('hello helen')
+
+#print('Hello,Python!')
+print('Hello,Python!')
+
+'''
+学习python的基本语法
+这是第四节的内容
+
+'''
+
+"""
+另外一种注释方法
+
+"""
+#行与缩进
+
+print('hello helen')
+print('hello helen')
+
+if True:
+    print("True")
+else:
+    print('False')
+
+if False:
+  print ("Answer")
+  print ("True")
+else:
+  print ("Answer")
+  print ("False")
+
+a=1
+b=2
+c=3
+
+total=a+b+c
+print("total=",total)
+
+str='Runabc'
+print(str[0:-1])
+print(str[2:])
+print(str[1:5:2])
+print(str*2)
+print(str+str)
+print('hello\nrunoob')
+print(r'hello\nrunoob')
