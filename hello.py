d8982849913d912d102096bdaec5efe64c899f16
8118557@qq.com <8118557@qq.com>
2020-12-03 21:03:22 +0800

    demo

--- hello.py
+++ hello.py
@@ -192,3 +192,118 @@ print(tinydict.values())
 
 Abc= {x: x**2 for x in (2, 4, 6)}
 print(Abc)
+
+
+print()
+a=21
+b=10
+c=0
+
+c=a+b
+print("1-c的值:",c)
+
+c=a-b
+print("2-c的值:",c)
+
+c=a*b
+print("3-c的值:",c)
+
+c=a/b
+print("4-c的值:",c)
+
+c=a%b
+print("5-c的值:",c)
+
+a=2
+b=3
+c=a**b
+print("6-c的值:",c)
+
+a=10
+b=5
+c=a//b
+print ("7 - c 的值为：", c)
+
+print()
+
+a = -60  # 60 = 0011 1100
+b = 13  # 13 = 0000 1101
+c = 0
+
+print(bin(60))
+print(bin(-60))
+print(format(60, '#b'))
+print(format(-60, 'b'))
+
+c = a & b  # 12 = 0000 1100
+print("1 - c 的值为：", c)
+
+c = a | b  # 61 = 0011 1101
+print("2 - c 的值为：", c)
+
+c = a ^ b  # 49 = 0011 0001
+print("3 - c 的值为：", c)
+
+c = ~a  # -61 = 1100 0011
+print("4 - c 的值为：", c)
+
+c = a << 2  # 240 = 1111 0000
+print("5 - c 的值为：", c)
+
+c = a >> 2  # 15 = 0000 1111
+print("6 - c 的值为：", c)
+
+print()
+
+a = 10
+b = 20
+
+if (a and b):
+    print("1 - 变量 a 和 b 都为 true")
+else:
+    print("1 - 变量 a 和 b 有一个不为 true")
+
+if (a or b):
+    print("2 - 变量 a 和 b 都为 true，或其中一个变量为 true")
+else:
+    print("2 - 变量 a 和 b 都不为 true")
+
+# 修改变量 a 的值
+a = 0
+if (a and b):
+    print("3 - 变量 a 和 b 都为 true")
+else:
+    print("3 - 变量 a 和 b 有一个不为 true")
+
+if (a or b):
+    print("4 - 变量 a 和 b 都为 true，或其中一个变量为 true")
+else:
+    print("4 - 变量 a 和 b 都不为 true")
+
+if not (a and b):
+    print("5 - 变量 a 和 b 都为 false，或其中一个变量为 false")
+else:
+    print("5 - 变量 a 和 b 都为 true")
+
+print()
+
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5]
+
+if (a in list):
+    print("1 - 变量 a 在给定的列表中 list 中")
+else:
+    print("1 - 变量 a 不在给定的列表中 list 中")
+
+if (b not in list):
+    print("2 - 变量 b 不在给定的列表中 list 中")
+else:
+    print("2 - 变量 b 在给定的列表中 list 中")
+
+# 修改变量 a 的值
+a = 2
+if (a in list):
+    print("3 - 变量 a 在给定的列表中 list 中")
+else:
+    print("3 - 变量 a 不在给定的列表中 list 中")
